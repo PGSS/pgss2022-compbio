643fe989525414b317962cca9548770b554d1d37
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2022-07-07 16:46:29 -0400

    example code now does digest

--- read_fasta_example.py
+++ read_fasta_example.py
@@ -1,6 +1,11 @@
 from fasta_reader import read_fasta
+from Bio.Seq import Seq
+from Bio.Restriction import *
+rb = RestrictionBatch(['MseI', 'Hpy188I'])
 
-
-for item in read_fasta("./lactic_acid_only.fa.gz"): #change this to wherever you have the file
-    print(item.defline)
+for item in read_fasta("C:/Users/AWESD/Downloads/lactic_acid_only.fa.gz"): #change this to wherever you have the file
+    print(item)
+    seq = Seq(item.sequence)
+    a = Analysis(rb, seq)
+    a.print_that()
     break #remove this to print everything in the database
