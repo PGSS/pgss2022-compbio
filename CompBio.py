a6684fd921167f99d1afa0a7bdede8b235d57db5
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2022-07-11 15:14:41 -0400

    Added framework for fragment functions

--- CompBio.py
+++ CompBio.py
@@ -1,5 +1,15 @@
-def is_match(gel_exp, gel_db):
-    #compares overall length to expected length
+def is_length(gel_exp, gel_db):
+    #compares overall length to database
     if abs(gel_exp["seq_length"]-gel_db["seq_length"]) > 50:
+        return False
+    return True
+def is_frag_msei(frag_exp, frag_db):
+    #compares MseI fragment digest to database
+    if 1 == 1:
+        return False
+    return True
+def is_frag_hpy188i(frag_exp, frag_db):
+    #compares Hpy188I fragment digest to database
+    if 1 == 1:
         return False
     return True
