51c25c18a0be7a41603b7576a085ab061b9efa48
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2022-07-14 16:57:01 -0400

    Added frag length check function
    Co-authored-by: DanielWang2305 <DanielWang2305@users.noreply.github.com>
    Co-authored-by: aidang56 <aidang56@users.noreply.github.com>
    Co-authored-by: Kurtis Zhang <KurtisZ18@users.noreply.github.com>

--- CMM.py
+++ CMM.py
@@ -4,7 +4,10 @@ import csv
 #replace with user input (also set +/- gap for seq len)
 sample_exp =  { "seq_length" : 560,   "fragments_MseI" : [481,200],  "fragments_Hpy188I" : [607,428,264]}
 sample_exp["fragments_MseI"] = [x for x in sample_exp["fragments_MseI"] if x <= sample_exp["seq_length"]]
+sample_exp["fragments_MseI"] = [x for x in sample_exp["fragments_MseI"] if x >= 100]
+
 sample_exp["fragments_Hpy188I"] = [x for x in sample_exp["fragments_Hpy188I"] if x <= sample_exp["seq_length"]]
+sample_exp["fragments_Hpy188I"] = [x for x in sample_exp["fragments_MseI"] if x >= 100]
 
 #FORMATS SEQUENCE LIST
 def db_import(db_path):
@@ -40,7 +43,7 @@ def seqlen_filter(gel_exp, gel_db, buffer=100):
             db_lenfilt.append(bacteria)
     return db_lenfilt
 
-#FRAGMENT MATCHING
+#FRAGMENT NUMBER MATCHING
 def fragnum_filter(gel_exp, gel_db, buffer=100):
     #If more bands in experimental than digital = not a match 
     #If more bands in the digital than the experimental, may be a match
@@ -52,7 +55,49 @@ def fragnum_filter(gel_exp, gel_db, buffer=100):
             db_fragfilt.append(bacteria)
     return db_fragfilt
 
-def fraglen_filter():
+
+#FRAGMENT LENGTH MATCHING
+def fraglen_filter(gel_exp, gel_db, buffer=100, range=50):
+    db_fraglenfilt = []
+    for bacteria in gel_db:
+        #removes fragments under 100 and sorts database by length
+        db_msei = [x for x in bacteria["fragments_MseI"] if x >= buffer]
+        db_msei.sort()
+        db_hpy188i = [x for x in bacteria["fragments_Hpy188I"] if x >= buffer]
+        db_hpy188i.sort()
+        exp_msei = gel_exp["fragments_MseI"]
+        exp_hpy188i = gel_exp["fragments_Hpy188I"]
+
+
+        potential_match = True
+        
+        for dbv in db_msei:
+
+            lower_range = dbv - range
+            upper_range = dbv + range
+            range_check = False
+            for expv in exp_msei:
+                if lower_range < expv and expv < upper_range:
+                    range_check = True
+            if range_check == False:
+                potential_match = False
+        
+        for dbv in db_hpy188i:
+
+            lower_range = dbv - range
+            upper_range = dbv + range
+            range_check = False
+            for expv in exp_hpy188i:
+                if lower_range < expv and expv < upper_range:
+                    range_check = True
+            if range_check == False:
+                potential_match = False
+        
+        
+        if potential_match == True:
+            db_fraglenfilt.append(bacteria)
+        
+    return db_fraglenfilt
     
     
 db_init = db_import('./Sequence_Analyses.csv')
@@ -61,5 +106,7 @@ db_lenfilt = seqlen_filter(sample_exp, db_init)
 print(len(db_lenfilt))
 db_fragfilt = fragnum_filter(sample_exp, db_lenfilt)
 print(len(db_fragfilt))
+db_fraglen = fraglen_filter(sample_exp, db_fragfilt)
+print(len(db_fraglen))
 
 
