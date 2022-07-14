8d15dae8286365b084cbfc3ab66fe0335645de72
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2022-07-14 14:36:03 -0400

    Basic model implementation
    Co-authored-by: DanielWang2305 <DanielWang2305@users.noreply.github.com>
    Co-authored-by: Malina <mreber23@users.noreply.github.com>
    Co-authored-by: Andrew McGuier <andrew@echogatetech.com>

--- CompBio.py
+++ CompBio.py
@@ -1,4 +1,4 @@
-sample_exp =  { "seq_length" : 560,   "fragments_MseI" : [481,200],  "fragments_Hpy188I" : [607, 428,264]}
+sample_exp =  { "seq_length" : 560,   "fragments_MseI" : [481,200],  "fragments_Hpy188I" : [607,428,264]}
 
 sample_db = {"defline" : "S002165652 Lactobacillus delbrueckii subsp. null; E9D2-1	Lineage=Root;rootrank;Bacteria;domain;Firmicutes;phylum;Bacilli;class;Lactobacillales;order;Lactobacillaceae;family;Lactobacillus;genus",
  "seq_length": 844,
@@ -7,15 +7,23 @@ sample_db = {"defline" : "S002165652 Lactobacillus delbrueckii subsp. null; E9D2
 
 def gel_match(gel_exp, gel_db):
     #compares overall length to database
-    if abs(gel_exp["seq_length"]-gel_db["seq_length"]) > 50:
-        return False
-    return True
+    length_is_match = abs(gel_exp["seq_length"]-gel_db["seq_length"]) > 50
+    mse_is_match = frags_match(gel_exp["fragments_MseI"], gel_db("fragments_Mse1"))
+    hpy_is_match = frags_match(gel_exp["fragments_Hpy188I"], gel_db["fragments_Hpy188I"])
+    return length_is_match and mse_is_match and hpy_is_match
+
+
+
+
+
+
 def frags_match(frag_exp, frag_db):
     #compares MseI fragment digest to database
     #exclude close to full length
     #exclude <100 bp from db
     #count remaining fragments
-    if 1 == 1:
+    if 1==1:
+    #if not frag_match(gelexp["fragments_MSeI"], gel_db["fragments_MSeI"]): <---dont know if this works or not
         return False
     return True
 
