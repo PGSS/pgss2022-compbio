c21f39b152fd7aa0efe19703dbeb5f046ae191a0
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2022-07-14 16:14:31 -0400

    Added fragment number filtering

--- CMM.py
+++ CMM.py
@@ -3,18 +3,11 @@ import csv
 
 #replace with user input (also set +/- gap for seq len)
 sample_exp =  { "seq_length" : 560,   "fragments_MseI" : [481,200],  "fragments_Hpy188I" : [607,428,264]}
+sample_exp["fragments_MseI"] = [x for x in sample_exp["fragments_MseI"] if x <= sample_exp["seq_length"]]
+sample_exp["fragments_Hpy188I"] = [x for x in sample_exp["fragments_Hpy188I"] if x <= sample_exp["seq_length"]]
 
-sample_db = {"defline" : "S002165652 Lactobacillus delbrueckii subsp. null; E9D2-1	Lineage=Root;rootrank;Bacteria;domain;Firmicutes;phylum;Bacilli;class;Lactobacillales;order;Lactobacillaceae;family;Lactobacillus;genus",
-"seq_length": 844,
-"fragments_MseI": [824, 20],
-"fragments_Hpy188I": [337, 36, 177, 38, 256]}
-
-
-
-
-#GEL MATCHING
-
-def db_cut(gel_exp, gel_db, buffer=50):
+#LENGTH MATCHING
+def db_len_filter(gel_exp, gel_db, buffer=100):
     db = []
     for bacteria in gel_db:
         if abs(gel_exp["seq_length"]-bacteria["seq_length"]) < buffer:
@@ -22,34 +15,27 @@ def db_cut(gel_exp, gel_db, buffer=50):
     return db
 
 
-def gel_match(gel_exp, gel_db, buffer=50):
-    #compares overall length to database
-    length_is_match = abs(gel_exp["seq_length"]-gel_db["seq_length"]) > buffer
-    mse_is_match = frags_match(gel_exp["fragments_MseI"], gel_db["fragments_MseI"])
-    hpy_is_match = frags_match(gel_exp["fragments_Hpy188I"], gel_db["fragments_Hpy188I"])
-    return length_is_match and mse_is_match and hpy_is_match
 
 
 
 #FRAGMENT MATCHING
+def frags_sort(frag_exp, frag_db):
+    #compares MseI fragment digest to database
+    #exclude close to full length
+    #exclude <100 bp from db
+    #count remaining fragments
+    if 1==1:
+    #if not frags_match(gelexp["fragments_MSeI"], gel_db["fragments_MSeI"]): 
+        return False
+    return True
 
+    if gel_exp["fragments_MSEI"]
 
-# def frags_match(frag_exp, frag_db):
-#     #compares MseI fragment digest to database
-#     #exclude close to full length
-#     #exclude <100 bp from db
-#     #count remaining fragments
-#     if 1==1:
-#     #if not frags_match(gelexp["fragments_MSeI"], gel_db["fragments_MSeI"]): 
-#         return False
-#     return True
-
-#     if gel_exp["fragments_MSEI"]
+def 
 
 
 
 #FORMATS SEQUENCE LIST
-
 def db_import(db_path):
     with open(db_path, 'r') as seqFile:
         
@@ -76,6 +62,7 @@ def db_import(db_path):
     return database
 
 db_init = db_import('./Sequence_Analyses.csv')
-db = db_cut(sample_exp, db_init)
+print(len(db_init))
+db = db_len_filter(sample_exp, db_init)
 print(len(db))
-#databs = db_import(db_init)
+
